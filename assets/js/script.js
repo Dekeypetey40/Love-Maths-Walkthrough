7ec9b12fe3a488bcdbd377e94cb62537145ccbfa
Dekeypetey40 <kmichaelmikhail@gmail.com>
2023-07-13 15:51:13 +0200

    Add checkanswer code with success and failure

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons){
         button.addEventListener("click", function(){
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked submit");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -34,9 +34,21 @@ function runGame(gameType) {
     }
 
 }
-
+/**
+ * Checks the answer against the first element in
+ * the returned calculatecorrectanswer array
+ */
 function checkAnswer () {
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
 
+    if (isCorrect) {
+        alert ("Hey! Nice jerb der pal! :)")
+    } else {
+        alert(`Nice try! you answered ${userAnswer}. The answer was ${calculatedAnswer[0]}!`);
+    }
+    runGame(calculatedAnswer[1]);
 }
 
 /**
